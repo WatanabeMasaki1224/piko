6d6e048549a9ed3bae6948b94ba876868c140a4a
masa101224 <masa101224@outlook.jp>
2025-06-23 23:22:17 +0900

    タイマー追加

---
+++ Assets/Script/GameTimer.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using TMPro; // TextMeshPro を使うために必要！
+
+public class GameTimer : MonoBehaviour
+{
+    public TMP_Text timerText;        // 経過時間表示用（TextMeshPro）
+    public GameObject clearPanel;     // クリアパネル
+    public TMP_Text clearTimeText;    // クリアタイム表示用（TextMeshPro）
+
+    private float timeElapsed = 0f;
+    private bool isTiming = false;
+
+    void Update()
+    {
+        if (isTiming)
+        {
+            timeElapsed += Time.deltaTime;
+            timerText.text = FormatTime(timeElapsed);
+        }
+    }
+
+    void Start()
+    {
+        StartTimer();  // 自動でタイマー開始
+    }
+
+    public void StartTimer()
+    {
+        isTiming = true;
+        timeElapsed = 0f;
+        clearPanel.SetActive(false);
+        Debug.Log("Start");
+    }
+
+    public void StopTimer()
+    {
+        isTiming = false;
+        clearPanel.SetActive(true);
+        clearTimeText.text = "クリアタイム：" + FormatTime(timeElapsed);
+    }
+
+    private string FormatTime(float time)
+    {
+        int minutes = Mathf.FloorToInt(time / 60f);
+        float seconds = time % 60f;
+        return string.Format("{0:00}:{1:00.00}", minutes, seconds);
+    }
+}
---
+++ Assets/Script/StageSerect.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StageSerect : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void  LoadStage1()
+    {
+        SceneManager.LoadScene("Stage1");
+    }
+
+    // Update is called once per frame
+    public void LoadStage2() 
+    {
+        SceneManager.LoadScene("Stage2");
+    }
+}
